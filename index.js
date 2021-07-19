aa79db07302854c4697d0064497b4b0462df858b
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-07-19 22:07:43 +0300

    Added Commands, config file, TOKEN IS INCLUDED

--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-const dotenv = require('dotenv');
-dotenv.config();
+const { prefix, TOKEN } = require("./config.json");
 
 
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// TODO: Mention in the console message
+// which bot is logged in
 client.once('ready', () => {
-    console.log("ready!");
+    console.log("Logged in!");
+});
+
+client.on("message", message=> {
+
+    if (message.content.startsWith(`${prefix}ping`)) {
+        message.channel.send("pong.");
+    } else if (message.content.startsWith(`${prefix}beep`)) {
+        message.channel.send("Boop.");
+    } else if(message.content == `${prefix}server`) {
+        message.channel.send(`This server's name is: ${message.guild.name}`);
+    }  
+
+
+
+
+
 });
 
 
 
-client.login(process.env.TOKEN);
+client.login(TOKEN);
